5b2cc0240257c1ea079f04e4ca059dd2b9cb065d
Kian <1226365769@qq.com>
2025-09-30 05:24:36 +1000

    IFN-9 User & Permission Classes

---
+++ backend/src/main/java/com/lcms/system/mapper/LawyersMapper.java
@@ -0,0 +1,12 @@
+package com.lcms.system.mapper;
+
+import com.lcms.system.entity.Lawyers;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author Kian
+ * @since 2025-09-30
+ */
+public interface LawyersMapper extends BaseMapper<Lawyers> {
+
+}
---
+++ backend/src/main/java/com/lcms/system/service/impl/LawyersServiceImpl.java
@@ -0,0 +1,12 @@
+package com.lcms.system.service.impl;
+
+import com.lcms.system.entity.Lawyers;
+import com.lcms.system.mapper.LawyersMapper;
+import com.lcms.system.service.LawyersService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LawyersServiceImpl extends ServiceImpl<LawyersMapper, Lawyers> implements LawyersService {
+
+}
